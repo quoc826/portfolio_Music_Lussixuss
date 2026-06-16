46087a627badd640ddc4ff90739c6fde278543f6
Trần Chí Quốc <quoct0447@gmail.com>
2026-06-16 20:46:16 +0700

    fixx

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD6Vp8ifPPfuim_x64_C2NGRZu1gZ3A0C0",
-  authDomain: "portfolio-lussixuss.firebaseapp.com",
-  projectId: "portfolio-lussixuss",
-  storageBucket: "portfolio-lussixuss.firebasestorage.app",
-  messagingSenderId: "741026510175",
-  appId: "1:741026510175:web:94387d2f9074710ae2f2ea",
-  measurementId: "G-TTRJWR7Z4C"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
